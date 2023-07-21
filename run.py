a05f2a79156703c321bb1ce0e4daf36dc34ba6c7
JF Abdo <jfabdo@gmail.com>
2023-07-21 13:43:23 -0500

    in color now

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def placeimg2(img1,img2,times):
         ycoord = randint(0,rows-rows2//FINALSIZE) 
         alpha = ycoord/rows
         size = round(INITIALSIZE * (1 - alpha) + FINALSIZE * alpha)
-        img3 = img2[0:rows2:size,0:cols2:size]
+        img3 = cv.resize(img2,(rows2//size, cols2//size))
 
         #choose the x coordinate
         rows3,cols3,channels3 = img3.shape
@@ -22,22 +22,23 @@ def placeimg2(img1,img2,times):
 
         #copy the new image to image 1
         img3gray = cv.cvtColor(img3,cv.COLOR_BGR2GRAY)
-        ret, mask = cv.threshold(img3gray, 10, 255, cv.THRESH_BINARY)
+        ret, mask = cv.threshold(img3gray, 0, 255, cv.THRESH_BINARY)
         mask_inv = cv.bitwise_not(mask)
         
         roi = img1[ycoord:ycoord + rows3, xcoord:xcoord + cols3]
+        print(xcoord,ycoord,rows3,cols3,size)
         img1_bg = cv.bitwise_and(roi,roi,mask = mask_inv)
         img3_fg = cv.bitwise_and(img3,img3,mask = mask)
-        dst = cv.add(img1_bg,img3_fg,dtype=cv.CV_64F)
+        dst = cv.add(img1_bg,img3_fg)
         img1[ycoord:ycoord + rows3, xcoord:xcoord + cols3] = dst
 
 spider = cv.imread('../../doodles/spider 2.png')
 assert spider is not None, "no spider pic"
 
 #np.zeros((500,height*30,3)
-canvas = np.zeros((500,500,3))
+canvas = np.zeros((500,500,3),dtype=np.uint8)
 
-placeimg2(canvas,spider, 3)
+placeimg2(canvas,spider, 100)
 
 cv.imshow('canvas',canvas)
 cv.waitKey(0)
