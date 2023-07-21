b6178141a35f075bdac957cdbfba99c341a078e9
JF Abdo <jfabdo@gmail.com>
2023-07-21 12:31:22 -0500

    changes size based on y coordinate

--- run.py
+++ run.py
@@ -1,22 +1,34 @@
 import cv2 as cv
 from random import randint
 
+INITIALSIZE = 2
+FINALSIZE = 20
+
 def placeimg2(img1,img2,times):
     rows,cols,channels = img1.shape
     rows2,cols2,channels2 = img2.shape
-    # roi = img1[0:-rows,0:-cols]
-    img2gray = cv.cvtColor(img2,cv.COLOR_BGR2GRAY)
-    ret, mask = cv.threshold(img2gray, 10, 255, cv.THRESH_BINARY)
-    mask_inv = cv.bitwise_not(mask)
+    
     for _ in range(times): 
-        #add a resizer to resize based on where it is in the picture
-        xcoord = randint(0,rows-rows2)
-        ycoord = randint(0,cols-cols2)
-        roi = img1[xcoord:xcoord+rows,ycoord:ycoord+cols]
+        #resizes based on where it is in the picture
+        ycoord = randint(0,rows-rows2//FINALSIZE) 
+        alpha = ycoord/rows
+        size = round(INITIALSIZE * (1 - alpha) + FINALSIZE * alpha)
+        img3 = img2[::size]
+
+        #choose the x coordinate
+        rows3,cols3,channels3 = img3.shape
+        xcoord = randint(0,cols - cols3)
+
+        #copy the new image to image 1
+        img3gray = cv.cvtColor(img3,cv.COLOR_BGR2GRAY)
+        ret, mask = cv.threshold(img3gray, 10, 255, cv.THRESH_BINARY)
+        mask_inv = cv.bitwise_not(mask)
+        
+        roi = img1[ycoord:ycoord + rows, xcoord:xcoord + cols]
         img1_bg = cv.bitwise_and(roi,roi,mask = mask_inv)
-        img2_fg = cv.bitwise_and(img2,img2,mask = mask)
-        dst = cv.add(img1_bg,img2_fg)
-        img1[0:rows, 0:cols ] = dst
+        img3_fg = cv.bitwise_and(img3,img3,mask = mask)
+        dst = cv.add(img1_bg,img3_fg)
+        img1[ycoord:ycoord + rows, xcoord:xcoord + cols] = dst
 
 
 
